344ea0b3599f73f28f173cc7143677904edf4a95
Pietro Incardona <incardon@mpi-cbg.de>
2015-07-12 01:25:32 +0200

    Fixing jenkins script for gin

--- build_device.sh
+++ build_device.sh
@@ -6,6 +6,10 @@ sh ./autogen.sh
 if [ "$2" == "master" ]
 then
  sh ./configure --disable-gpu
+elif [ "$2" == "gin" ]
+ module load gcc/4.9.2
+ module load boost/1.54.0
+ sh ./configure
 else
  sh ./configure
 fi
