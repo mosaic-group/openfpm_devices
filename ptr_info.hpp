e0a5f077afd50b987d4317167c559e6e900fc8c7
Pietro Incardona <i-bird@localhost.localdomain>
2015-12-23 20:29:49 -0500

    Add missing files

---
+++ ptr_info.hpp
@@ -0,0 +1,21 @@
+/*
+ * ptr_info.hpp
+ *
+ *  Created on: Nov 19, 2015
+ *      Author: i-bird
+ */
+
+#ifndef OPENFPM_DEVICES_SRC_PTR_INFO_HPP_
+#define OPENFPM_DEVICES_SRC_PTR_INFO_HPP_
+
+
+struct ptr_info
+{
+	size_t id;
+	size_t struct_id;
+	size_t project_id;
+	size_t size;
+};
+
+
+#endif /* OPENFPM_DEVICES_SRC_PTR_INFO_HPP_ */
