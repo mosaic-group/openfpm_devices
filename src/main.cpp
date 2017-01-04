a0b02db5938003755b85c86fded64b107ac4e55d
Pietro Incardona <incardon@mpi-cbg.de>
2017-01-04 12:09:54 +0100

    Fixing Version number

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-
-#include "config.h"
 #define BOOST_TEST_MODULE "C++ test module for OpenFPM_data project"
 #include <boost/test/included/unit_test.hpp>
 
+#include "config.h"
 #include "memory/HeapMemory_unit_tests.hpp"
 
