5dec602a209956a0820b2934449fde684c881caa
Pietro Incardona <incardon@mpi-cbg.de>
2019-01-02 23:43:08 +0100

    Fixing boost unit test

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "config.h"
-#define BOOST_TEST_MODULE "C++ test module for OpenFPM_data project"
-#include <boost/test/included/unit_test.hpp>
-
-#include "memory/HeapMemory_unit_tests.hpp"
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-#define BOOST_TEST_MODULE "C++ test module for OpenFPM_data project"
-#include <boost/test/included/unit_test.hpp>
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+// initialization function:
+bool init_unit_test()
+{
+  return true;
+}
+
+// entry point:
+int main(int argc, char* argv[])
+{
+  return boost::unit_test::unit_test_main( &init_unit_test, argc, argv );
+}
 
 #include "config.h"
 #include "memory/Memory_unit_tests.hpp"
