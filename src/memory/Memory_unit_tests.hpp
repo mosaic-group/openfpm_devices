479ae6962c0ad4e907595569b5e70aa6a8e6ce47
Aryaman Gupta <argupta@mpi-cbg.de>
2020-08-23 20:26:37 +0200

    creates shared memory tests for HeapMemory

--- src/memory/Memory_unit_tests.hpp
+++ src/memory/Memory_unit_tests.hpp
@@ -12,6 +12,8 @@
 
 #include "memory/HeapMemory.hpp"
 #include "memory/BHeapMemory.hpp"
+#include "memory/ShmBuffer.hpp"
+#include "timer.hpp"
 #ifdef CUDA_GPU
 #include "memory/CudaMemory.cuh"
 #endif
@@ -85,6 +87,7 @@ template<typename T> void test()
 	dst.copy(src);
 	dst.deviceToHost();
 
+    unsigned char * ptr2 = (unsigned char *)dst.getPointer();
 	for (size_t i = 0 ; i < FIRST_ALLOCATION ; i++)
 	{
 		unsigned char c=i;
@@ -124,6 +127,276 @@ template<typename T> void test()
 	BOOST_REQUIRE_EQUAL(mem.size(),FIRST_ALLOCATION);
 
 	}
+}
+
+template<typename T> void test_shm()
+{
+    //! [Allocate some memory and fill with data]
+    T mem;
+    handle_shmem h = create_shmanager().create("/", 1);
+    mem.set_shmem_handle(h);
+
+    BOOST_REQUIRE_EQUAL(mem.size(),0ul);
+
+    mem.allocate(FIRST_ALLOCATION);
+
+    ShmBuffer *memBuf;
+
+    memBuf = new ShmBuffer("/", 1, false);
+    ptr_with_size pws{};
+
+    bool finish = false;
+    size_t bufSize = 0;
+
+    std::thread th1([memBuf, &pws, &finish, &bufSize]()
+    {
+        while(!finish) {
+            memBuf->update_key(true);
+            pws = memBuf->attach();
+            bufSize = pws.size - extra_pad - MEM_ALIGNMENT;
+            memBuf->detach(false);
+        }
+    });
+
+    timer t;
+    t.start();
+    while(bufSize != FIRST_ALLOCATION)
+    {
+        if(t.getwct()>2.0)
+        {
+            break;
+        }
+    }
+
+    BOOST_REQUIRE_EQUAL(mem.size(),FIRST_ALLOCATION);
+    BOOST_REQUIRE_EQUAL(mem.size(),bufSize);
+
+    std::cout<<"Checked the first allocation"<<std::endl;
+
+    // get the pointer of the allocated memory and fill
+
+    unsigned char * ptr = (unsigned char *)mem.getPointer();
+    for (size_t i = 0 ; i < mem.size() ; i++)
+    {ptr[i] = i;}
+
+    mem.hostToDevice();
+
+    //! [Allocate some memory and fill with data]
+
+    //! [Resize the memory]
+    mem.resize(SECOND_ALLOCATION);
+
+//    memBuf->update_key(true);
+//    std::cout<<"Updated key"<<std::endl;
+//    pws = memBuf->attach();
+//    std::cout<<"Attached"<<std::endl;
+
+    t.start();
+    while(bufSize != SECOND_ALLOCATION)
+    {
+        if(t.getwct()>2.0)
+        {
+            break;
+        }
+    }
+
+    unsigned char * ptr2 = (unsigned char *)mem.getPointer();
+
+    BOOST_REQUIRE_EQUAL(mem.size(),SECOND_ALLOCATION);
+    BOOST_REQUIRE_EQUAL(mem.size(),bufSize);
+    BOOST_REQUIRE_EQUAL(mem.isInitialized(),false);
+
+    //! [Resize the memory]
+
+    // check that the data are retained
+    for (size_t i = 0 ; i < FIRST_ALLOCATION ; i++)
+    {
+        unsigned char c = i;
+        BOOST_REQUIRE_EQUAL(ptr2[i],c);
+    }
+
+    //! [Shrink memory]
+
+    mem.resize(1);
+
+    sleep(2);
+
+    BOOST_REQUIRE_EQUAL(mem.size(),SECOND_ALLOCATION);
+    BOOST_REQUIRE_EQUAL(mem.size(),bufSize);
+    std::cout<<"Passed first shrink mem test"<<std::endl;
+
+    //! [Shrink memory]
+
+
+    {
+        //! [Copy memory]
+
+        handle_shmem h2 = create_shmanager().create("/", 2);
+        handle_shmem h3 = create_shmanager().create("/", 3);
+
+        T src;
+        src.set_shmem_handle(h2);
+        T dst;
+        dst.set_shmem_handle(h3);
+
+        src.allocate(FIRST_ALLOCATION);
+        dst.allocate(SECOND_ALLOCATION);
+
+        unsigned char * ptr = (unsigned char *)src.getPointer();
+        for (size_t i = 0 ; i < src.size() ; i++)
+        {ptr[i] = i;}
+
+        src.hostToDevice();
+        dst.copy(src);
+        dst.deviceToHost();
+
+        unsigned char * ptr2 = (unsigned char *)dst.getPointer();
+        for (size_t i = 0 ; i < FIRST_ALLOCATION ; i++)
+        {
+            unsigned char c=i;
+            BOOST_REQUIRE_EQUAL(ptr2[i],c);
+        }
+
+        ShmBuffer *buf2;
+        buf2 = new ShmBuffer("/", 2, false);
+        ptr_with_size pws2{};
+
+        buf2->update_key(true);
+        pws2 = buf2->attach();
+
+        ShmBuffer *buf3;
+        buf3 = new ShmBuffer("/", 3, false);
+        ptr_with_size pws3{};
+
+        buf3->update_key(true);
+        pws3 = buf3->attach();
+
+        BOOST_REQUIRE_EQUAL(pws2.size - MEM_ALIGNMENT - extra_pad, FIRST_ALLOCATION);
+        BOOST_REQUIRE_EQUAL(pws3.size - MEM_ALIGNMENT - extra_pad, SECOND_ALLOCATION);
+
+        //! [Copy Memory]
+    }
+
+    {
+        handle_shmem h4 = create_shmanager().create("/", 4);
+
+        T src;
+        src.set_shmem_handle(h4);
+        src.allocate(FIRST_ALLOCATION);
+
+        unsigned char * ptr = (unsigned char *)src.getPointer();
+        for (size_t i = 0 ; i < src.size() ; i++)
+        {ptr[i] = i;}
+
+        src.hostToDevice();
+        T dst = src;
+        dst.deviceToHost();
+
+        unsigned char * ptr2 = (unsigned char *)dst.getPointer();
+
+        for (size_t i = 0 ; i < FIRST_ALLOCATION ; i++)
+        {
+            unsigned char c=i;
+            BOOST_REQUIRE_EQUAL(ptr2[i],c);
+        }
+
+        mem.destroy();
+
+        int tok1 = PROJ_ID(1, 0);
+        int tok2  =PROJ_ID(1, 1);
+
+        int key1 = ftok("/", tok1);
+        int key2 = ftok("/", tok2);
+
+        int shmid1;
+        int shmid2;
+        int errno1 = 0;
+        int errno2 = 0;
+
+        t.start();
+        while(errno1 != ENOENT && errno2 != ENOENT)
+        {
+            shmid1 = shmget(key1, 0, 0);
+            errno1 = errno;
+            shmid2 = shmget(key2, 0, 0);
+            errno2 = errno;
+            if(t.getwct()>2.0)
+            {
+                break;
+            }
+        }
+
+        BOOST_REQUIRE_EQUAL(mem.size(),0ul);
+        BOOST_REQUIRE_EQUAL(errno1,ENOENT);
+        BOOST_REQUIRE_EQUAL(errno2,ENOENT);
+
+        mem.allocate(FIRST_ALLOCATION);
+
+        t.start();
+        while(bufSize != FIRST_ALLOCATION)
+        {
+            if(t.getwct()>2.0)
+            {
+                break;
+            }
+        }
+        BOOST_REQUIRE_EQUAL(mem.size(),FIRST_ALLOCATION);
+        BOOST_REQUIRE_EQUAL(bufSize,FIRST_ALLOCATION);
+        finish = true;
+        th1.join();
+    }
+}
+
+void shm_usage()
+{
+    //! [Create a shared memory unit and fill it with data]
+    handle_shmem h = create_shmanager().create("/", 0);
+    unsigned char * ptr = (unsigned char*)create_shmanager().alloc(h, FIRST_ALLOCATION);
+
+    for(size_t i = 0; i < FIRST_ALLOCATION; i++)
+    {
+        ptr[i] = i;
+    }
+
+    //! [Create a reader and read memory]
+
+    ShmBuffer * buf = new ShmBuffer("/", 0, false);
+    ptr_with_size pws{};
+
+    buf->update_key(true);
+    pws = buf->attach();
+    buf->detach(false); // Detach from previous memory. Currently, no previous memory, so will do nothing
+
+    BOOST_REQUIRE_EQUAL(pws.size, FIRST_ALLOCATION);
+    unsigned char * ptr2 = (unsigned char*)pws.ptr;
+    for(size_t i = 0; i < FIRST_ALLOCATION; i++)
+    {
+        unsigned char c = i;
+        BOOST_REQUIRE_EQUAL(ptr2[i], c);
+    }
+
+    //! [Allocate a larger unit of memory with the same shared memory token and fill it with data]
+
+    unsigned char * ptr3 = (unsigned char*)create_shmanager().alloc(h, SECOND_ALLOCATION);
+    create_shmanager().free(h, ptr);
+
+    for(size_t i = 0; i < SECOND_ALLOCATION; i++)
+    {
+        ptr3[i] = i;
+    }
+
+    //! [Read from the larger unit of memory]
+    buf->update_key(true);
+    pws = buf->attach();
+    buf->detach(false); // Signals that the consumer has finished reading from the previous allocation and it can now be deleted
+
+    BOOST_REQUIRE_EQUAL(pws.size, SECOND_ALLOCATION);
+    unsigned char * ptr4 = (unsigned char*)pws.ptr;
+    for(size_t i = 0; i < SECOND_ALLOCATION; i++)
+    {
+        unsigned char c = i;
+        BOOST_REQUIRE_EQUAL(ptr4[i], c);
+    }
 }
 
 template<typename T> void Btest()
@@ -264,6 +537,16 @@ BOOST_AUTO_TEST_CASE( use_heap_memory )
 #ifdef CUDA_GPU
 	test<CudaMemory>();
 #endif
+}
+
+BOOST_AUTO_TEST_CASE( use_shared_memory )
+{
+        test_shm<HeapMemory>();
+}
+
+BOOST_AUTO_TEST_CASE( shm_example )
+{
+        shm_usage();
 }
 
 BOOST_AUTO_TEST_CASE( use_memory )
