0f02536f28319252481cd7f2cee683f3128d8060
Incardona Pietro <incardon@mpi-cbg.de>
2021-11-16 10:38:11 +0100

    Adding missing backend

---
+++ src/util/cudify/cudify_cuda.hpp
@@ -0,0 +1,106 @@
+#ifndef __CUDIFY_CUDA_HPP__
+#define __CUDIFY_CUDA_HPP__
+
+#define CUDA_ON_BACKEND CUDA_BACKEND_CUDA
+
+constexpr int default_kernel_wg_threads_ = 1024;
+
+#if CUDART_VERSION >= 11000 && defined(__NVCC__)
+    #include "cub/util_type.cuh"
+    #include "cub/block/block_scan.cuh"
+#endif
+
+static void init_wrappers()
+{}
+
+#if defined(SE_CLASS1) || defined(CUDA_CHECK_LAUNCH)
+
+#define CUDA_LAUNCH(cuda_call,ite, ...) \
+        {\
+        cudaDeviceSynchronize(); \
+        {\
+            cudaError_t e = cudaGetLastError();\
+            if (e != cudaSuccess)\
+            {\
+                std::string error = cudaGetErrorString(e);\
+                std::cout << "Cuda an error has occurred before this CUDA_LAUNCH, detected in: " << __FILE__ << ":" << __LINE__ << " " << error << std::endl;\
+            }\
+        }\
+        CHECK_SE_CLASS1_PRE\
+        if (ite.wthr.x != 0)\
+        {cuda_call<<<ite.wthr,ite.thr>>>(__VA_ARGS__);}\
+        cudaDeviceSynchronize(); \
+        {\
+            cudaError_t e = cudaGetLastError();\
+            if (e != cudaSuccess)\
+            {\
+                std::string error = cudaGetErrorString(e);\
+                std::cout << "Cuda Error in: " << __FILE__ << ":" << __LINE__ << " " << error << std::endl;\
+            }\
+            CHECK_SE_CLASS1_POST(#cuda_call,__VA_ARGS__)\
+        }\
+        }
+
+#define CUDA_LAUNCH_DIM3(cuda_call,wthr,thr, ...) \
+        {\
+        cudaDeviceSynchronize(); \
+        {\
+            cudaError_t e = cudaGetLastError();\
+            if (e != cudaSuccess)\
+            {\
+                std::string error = cudaGetErrorString(e);\
+                std::cout << "Cuda an error has occurred before this CUDA_LAUNCH, detected in: " << __FILE__ << ":" << __LINE__ << " " << error << std::endl;\
+            }\
+        }\
+        CHECK_SE_CLASS1_PRE\
+        cuda_call<<<wthr,thr>>>(__VA_ARGS__);\
+        cudaDeviceSynchronize(); \
+        {\
+            cudaError_t e = cudaGetLastError();\
+            if (e != cudaSuccess)\
+            {\
+                std::string error = cudaGetErrorString(e);\
+                std::cout << "Cuda Error in: " << __FILE__ << ":" << __LINE__ << " " << error << std::endl;\
+            }\
+            CHECK_SE_CLASS1_POST(#cuda_call,__VA_ARGS__)\
+        }\
+        }
+
+#define CUDA_CHECK() \
+        {\
+        cudaDeviceSynchronize(); \
+        {\
+            cudaError_t e = cudaGetLastError();\
+            if (e != cudaSuccess)\
+            {\
+                std::string error = cudaGetErrorString(e);\
+                std::cout << "Cuda an error has occurred before, detected in: " << __FILE__ << ":" << __LINE__ << " " << error << std::endl;\
+            }\
+        }\
+        CHECK_SE_CLASS1_PRE\
+        cudaDeviceSynchronize(); \
+        {\
+            cudaError_t e = cudaGetLastError();\
+            if (e != cudaSuccess)\
+            {\
+                std::string error = cudaGetErrorString(e);\
+                std::cout << "Cuda Error in: " << __FILE__ << ":" << __LINE__ << " " << error << std::endl;\
+            }\
+            CHECK_SE_CLASS1_POST("no call","no args")\
+        }\
+        }
+
+#else
+
+#define CUDA_LAUNCH(cuda_call,ite, ...) \
+        if (ite.wthr.x != 0)\
+        {cuda_call<<<ite.wthr,ite.thr>>>(__VA_ARGS__);}
+
+#define CUDA_LAUNCH_DIM3(cuda_call,wthr,thr, ...) \
+        cuda_call<<<wthr,thr>>>(__VA_ARGS__);
+
+#define CUDA_CHECK()
+
+#endif
+
+#endif
